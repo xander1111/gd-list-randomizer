6db9c464f904e74aff9fab34562c835aca3dc3f0
Xander Bailey <xander.bailey@gmail.com>
2026-08-25 21:37:59 -0700

    Add generic background to WheelLayer

--- src/WheelLayer.cpp
+++ src/WheelLayer.cpp
@@ -31,6 +31,45 @@ bool WheelLayer::init()
 
     const CCSize winSize = CCDirector::sharedDirector()->getWinSize();
 
+    // Background
+    //
+    // Recreates the background commonly used by the game for menus
+    CCSprite* background = CCSprite::create("GJ_gradientBG-hd.png");
+    background->setID("background"_spr);
+
+    const CCSize bgSize = background->getContentSize();
+
+    background->setScaleX((winSize.width + 10) / bgSize.width);
+    background->setScaleY((winSize.height + 10) / bgSize.height);
+
+    background->setAnchorPoint({0, 0});
+    background->setPosition({-5,-5});
+
+    background->setColor({.r = 0, .g = 102, .b = 255});
+
+    background->setZOrder(-2);
+    addChild(background);
+
+
+    // Corner decorations
+    CCSprite* leftCorner = CCSprite::createWithSpriteFrameName("GJ_sideArt_001.png");
+    CCSprite* rightCorner = CCSprite::createWithSpriteFrameName("GJ_sideArt_001.png");
+
+    leftCorner->setID("left-corner"_spr);
+    rightCorner->setID("right-corner"_spr);
+
+    leftCorner->setAnchorPoint({0, 0});
+    rightCorner->setAnchorPoint({1, 0});
+
+    leftCorner->setPosition({-1,-1});
+    rightCorner->setPosition({winSize.width + 1,-1});
+
+    rightCorner->setFlipX(true);
+
+    addChild(leftCorner);
+    addChild(rightCorner);
+
+
     // Back button
     CCMenuItemSpriteExtra* exitButton = CCMenuItemSpriteExtra::create(
         CCSprite::createWithSpriteFrameName("GJ_arrow_01_001.png"),
